845a20452dbd6594b766a9bd5787cf7e298db4a9
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2024-04-25 06:01:34 +0800

    Add more buttons to app

--- main.ts
+++ main.ts
@@ -1,34 +1,20 @@
 // A very simple web socket client
-/*
-function getClientHtml(domain) {
-  return `<html>
-    <body>
-      <iframe id="iframe1"title="PVA - Time vs Day"></iframe>
-      <script>
-        document.getElementById('iframe1').contentWindow.document.write("<html><body>Hello world</body></html>");
-      </script>
-    </body>
-  </html>`;
-}*/
-const means = await Deno.readTextFile("./app_assets/pages/means.html");
-const move_durations = await Deno.readTextFile("./app_assets/pages/idle_durations.html");
-const time_discretized = await Deno.readTextFile("./app_assets/pages/time_discretized.html");
-const time_undiscretized = await Deno.readTextFile("./app_assets/pages/time_undiscretized.html");
-const pva_time_day= await Deno.readTextFile("./app_assets/pages/pva_time_day.html");
-const pva_path_time = await Deno.readTextFile("./app_assets/pages/pva_path_time.html");
-const pva_path_day = await Deno.readTextFile("./app_assets/pages/pva_path_day.html");
-const RandomForestRegressor_prediction = await Deno.readTextFile("./app_assets/pages/RandomForestRegressor_prediction.html");
-const KNearestNeighborsRegressor_prediction = await Deno.readTextFile("./app_assets/pages/KNearestNeighborsRegressor_prediction.html");
-const DecisionTreeRegressor_prediction = await Deno.readTextFile("./app_assets/pages/DecisionTreeRegressor_prediction.html");
-const ANNPredictiveModel_prediction = await Deno.readTextFile("./app_assets/pages/ANNPredictiveModel_prediction.html");
-
-// A very simple web socket client
 function getClientHtml(domain) {
   return `<html>
     <body>
       <div id="controls" height="10%" width="100%">
-        <button id="loader" onclick="ws.send(0)">AAA</button>
-        <button id="loader2">AAA</button>
+        <button onclick="ws.send(0)">Mean Move Durations (by path)</button>
+        <button onclick="ws.send(1)">Idle Durations</button>
+        <button onclick="ws.send(2)">Move Durations</button>
+        <button onclick="ws.send(3)">Time (Discritized)</button>
+        <button onclick="ws.send(4)">Time (Undiscritized)</button>
+        <button onclick="ws.send(5)">Surface (Path/Time)</button>
+        <button onclick="ws.send(6)">Surface (Path/Day)</button>
+        <button onclick="ws.send(7)">Surface (Time/Day)</button>
+        <button onclick="ws.send(8)">Evaluation (DT)</button>
+        <button onclick="ws.send(9)">Evaluation (RF)</button>
+        <button onclick="ws.send(10)">Evaluation (KNN)</button>
+        <button onclick="ws.send(11)">Evaluation (ANN)</button>
       </div>
       <div id="the_output" height="90%" width="100%">
         <iframe id="the_iframe" height="100%" width="100%" title="Dashboard"></iframe>
@@ -36,21 +22,28 @@ function getClientHtml(domain) {
       <script>
         const ws = new WebSocket("wss://${domain}/");
         const output = document.querySelector("#the_output");
-        const loader = document.querySelector("#loader");
         const write = (msg) => {
           output.innerHTML = '<iframe id="the_iframe" height="100%" width="100%" title="Dashboard"></iframe>';
-          let the_iframe = document.querySelector("#the_iframe");
-          the_iframe.contentWindow.document.write(msg);
+          document.querySelector("#the_iframe").contentWindow.document.write(msg);
         }
-
-        // Print a "pong" when the server responds
         ws.onmessage = (e) => write(e.data);
-
-        // Ping the server every second
       </script>
     </body>
   </html>`;
 }
+
+const means = await Deno.readTextFile("./app_assets/pages/means.html");
+const idle_durations = await Deno.readTextFile("./app_assets/pages/idle_durations.html");
+const move_durations = await Deno.readTextFile("./app_assets/pages/move_durations.html");
+const time_discretized = await Deno.readTextFile("./app_assets/pages/time_discretized.html");
+const time_undiscretized = await Deno.readTextFile("./app_assets/pages/time_undiscretized.html");
+const pva_path_time = await Deno.readTextFile("./app_assets/pages/pva_path_time.html");
+const pva_path_day = await Deno.readTextFile("./app_assets/pages/pva_path_day.html");
+const pva_time_day= await Deno.readTextFile("./app_assets/pages/pva_time_day.html");
+const DecisionTreeRegressor_prediction = await Deno.readTextFile("./app_assets/pages/DecisionTreeRegressor_prediction.html");
+const RandomForestRegressor_prediction = await Deno.readTextFile("./app_assets/pages/RandomForestRegressor_prediction.html");
+const KNearestNeighborsRegressor_prediction = await Deno.readTextFile("./app_assets/pages/KNearestNeighborsRegressor_prediction.html");
+const ANNPredictiveModel_prediction = await Deno.readTextFile("./app_assets/pages/ANNPredictiveModel_prediction.html");
 
 // A simple WebSocket server
 Deno.serve((request: Request) => {
@@ -60,11 +53,19 @@ Deno.serve((request: Request) => {
 
     // Listen for incoming messages
     socket.onmessage = (_e) => {
-      console.log("ping");
-      if (_e.data==0)
-        socket.send(pva_time_day);
-      else
-        socket.send("nah");
+      if (_e.data==0) socket.send(means);
+      else if (_e.data==1) socket.send(idle_durations);
+      else if (_e.data==2) socket.send(move_durations);
+      else if (_e.data==3) socket.send(time_discretized);
+      else if (_e.data==4) socket.send(time_undiscretized);
+      else if (_e.data==5) socket.send(pva_path_time);
+      else if (_e.data==6) socket.send(pva_path_day);
+      else if (_e.data==7) socket.send(pva_time_day);
+      else if (_e.data==8) socket.send(DecisionTreeRegressor_prediction);
+      else if (_e.data==9) socket.send(RandomForestRegressor_prediction);
+      else if (_e.data==10) socket.send(KNearestNeighborsRegressor_prediction);
+      else if (_e.data==11) socket.send(ANNPredictiveModel_prediction);
+      else socket.send("nah");
     };
 
     return response;
